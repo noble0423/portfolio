0a88d6a94601733643891256e650e87a4bc3abb3
noble0423 <ryan.noble82@gmail.com>
2019-04-26 20:55:40 -0500

    trying new PHP attmepts.

--- assets/php/contactForm.php
+++ assets/php/contactForm.php
@@ -10,6 +10,7 @@
 	// 	$subject = $_POST['c_subject'];
 	// 	$message = $_POST['c_message'];
 
+
 	// 	if (mail($to, $subject, $message, $from)) {
 	// 		$result = array(
 	// 			'message' => 'Thanks for contacting me!',
@@ -27,28 +28,27 @@
 
 	// Contact
 	$to = 'ryan.noble82@gmail.com';
-	$from = "ryan@ryanmnoble.com";
 	// $subject = 'Subject here...';
 
-	if(isset($_POST['c_name']) && isset($_POST['c_email']) && isset($_POST['c_subject']) && isset($_POST['c_message'])){
+	// if(isset($_POST['c_name']) && isset($_POST['c_email']) && isset($_POST['c_subject']) && isset($_POST['c_message'])){
 		$name    = $_POST['c_name'];
-		$email    = $_POST['c_email'];
+		$from    = $_POST['c_email'];
 		$subject = $_POST['c_subject'];
 		$message = $_POST['c_message'];
-
-		if (mail($to, $subject, $message, $email)) {
-			$result = array(
-				'message' => 'Thanks for contacting me!',
-				'sendstatus' => 1
-				);
-			echo json_encode($result);
-		} else {
-			$result = array(
-				'message' => 'Sorry, something is wrong',
-				'sendstatus' => 1
-				);
-			echo json_encode($result);
-		}
-	}
-
+		
+		mail($to, $subject, $message, $from);
+		// if (mail($to, $subject, $message, $from)) {
+		// 	$result = array(
+		// 		'message' => 'Thanks for contacting me!',
+		// 		'sendstatus' => 1
+		// 		);
+		// 	echo json_encode($result);
+		// } else {
+		// 	$result = array(
+		// 		'message' => 'Sorry, something is wrong',
+		// 		'sendstatus' => 1
+		// 		);
+		// 	echo json_encode($result);
+		// }
+	// }
 ?>
