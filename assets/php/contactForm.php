85a01bd14da08c8810b31c5b0667511f2c144d28
noble0423 <ryan.noble82@gmail.com>
2019-04-26 21:23:25 -0500

    reverted back to orig

--- assets/php/contactForm.php
+++ assets/php/contactForm.php
@@ -1,55 +1,28 @@
 <?php
 
-	// // Contact
-	// $to = 'ryan.noble82@gmail.com';
-	// // $subject = 'Subject here...';
-
-	// if(isset($_POST['c_name']) && isset($_POST['c_email']) && isset($_POST['c_subject']) && isset($_POST['c_message'])){
-	// 	$name    = $_POST['c_name'];
-	// 	$from    = $_POST['c_email'];
-	// 	$subject = $_POST['c_subject'];
-	// 	$message = $_POST['c_message'];
-
-
-	// 	if (mail($to, $subject, $message, $from)) {
-	// 		$result = array(
-	// 			'message' => 'Thanks for contacting me!',
-	// 			'sendstatus' => 1
-	// 			);
-	// 		echo json_encode($result);
-	// 	} else {
-	// 		$result = array(
-	// 			'message' => 'Sorry, something is wrong',
-	// 			'sendstatus' => 1
-	// 			);
-	// 		echo json_encode($result);
-	// 	}
-	// }
-
 	// Contact
 	$to = 'ryan.noble82@gmail.com';
 	// $subject = 'Subject here...';
 
-	// if(isset($_POST['c_name']) && isset($_POST['c_email']) && isset($_POST['c_subject']) && isset($_POST['c_message'])){
+	if(isset($_POST['c_name']) && isset($_POST['c_email']) && isset($_POST['c_subject']) && isset($_POST['c_message'])){
 		$name    = $_POST['c_name'];
 		$from    = $_POST['c_email'];
 		$subject = $_POST['c_subject'];
 		$message = $_POST['c_message'];
-		$headers = "From: webmaster@example.com" . "\r\n" .
-		
-		mail($to, $subject, $message, $headers, $from);
-		// if (mail($to, $subject, $message, $from)) {
-		// 	$result = array(
-		// 		'message' => 'Thanks for contacting me!',
-		// 		'sendstatus' => 1
-		// 		);
-		// 	echo json_encode($result);
-		// } else {
-		// 	$result = array(
-		// 		'message' => 'Sorry, something is wrong',
-		// 		'sendstatus' => 1
-		// 		);
-		// 	echo json_encode($result);
-		// }
-	// }
+
+		if (mail($to, $subject, $message, $from)) {
+			$result = array(
+				'message' => 'Thanks for contacting me!',
+				'sendstatus' => 1
+				);
+			echo json_encode($result);
+		} else {
+			$result = array(
+				'message' => 'Sorry, something is wrong',
+				'sendstatus' => 1
+				);
+			echo json_encode($result);
+		}
+	}
+
 ?>
