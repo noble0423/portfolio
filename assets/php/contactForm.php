c562f8b263937a4175b316d55297cff438d32c9e
noble0423 <ryan.noble82@gmail.com>
2019-09-18 10:50:31 -0500

    added headers to contactForm.php in order to attempt to solve sent from info once the email actually sends (it is currently sending from the server address.

--- assets/php/contactForm.php
+++ assets/php/contactForm.php
@@ -2,15 +2,19 @@
 
 	// Contact
 	$to = 'ryan.noble82@gmail.com';
+	
 	// $subject = 'Subject here...';
 
 	if(isset($_POST['c_name']) && isset($_POST['c_email']) && isset($_POST['c_subject']) && isset($_POST['c_message'])){
-		$name    = $_POST['c_name'];
-		$from    = $_POST['c_email'];
-		$subject = $_POST['c_subject'];
-		$message = $_POST['c_message'];
+		$name 		= $_POST['c_name'];
+		$from    	= $_POST['c_email'];
+		$subject 	= $_POST['c_subject'];
+		$message 	= $_POST['c_message'];
+		$headers 	= "MIME-VERSION: 1.0" . "\r\n";
+		$headers   .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+		$headers	= "From : <$from>\r\n ";
 
-		if (mail($to, $subject, $message, $from)) {
+		if (mail($to, $subject, $message, $headers)) {
 			$result = array(
 				'message' => 'Thanks for contacting me!',
 				'sendstatus' => 1
