38e71bd7a4144d8e771e469424a921ce1ff1e741
noble0423 <ryan.noble82@gmail.com>
2019-09-19 10:57:42 -0500

    made small formatting changes to contactForm.php where I am now sending a HTML email.

--- assets/php/contactForm.php
+++ assets/php/contactForm.php
@@ -6,12 +6,33 @@
 	if(isset($_POST['c_name']) && isset($_POST['c_email']) && isset($_POST['c_subject']) && isset($_POST['c_message'])){
 		$name 		= $_POST['c_name'];
 		$from    	= $_POST['c_email'];
-		$message 	= $_POST['c_message'];
-		$message   .= "\r\n" . "\r\n" . "Client's email address: {$from}";
+
+		$message	= "
+					<html>
+					<head>
+					<title>HTML EMAIL</title>
+					</head>
+					<body>
+					<h4>The following message was sent from the Contact Form on your Portfolio Website:</h4>
+					<p>
+					";
+		$message   .= $_POST['c_message'];
+		$message   .= "
+					</p>
+					<br>
+					<p><u>Customer Name: </u> {$name}<br>
+					*** Hitting <i>'reply'</i> will send a reply email to the following address: {$from}</p>
+					</body>
+					</html>
+					";
+
 		$subject	= "Website Inquiry - '"; 
 		$subject   .= $_POST['c_subject'];
 		$subject   .= "'";
-		$headers	= "From: {$name} <no-reply@ryanmnoble.com>" . "\r\n";
+
+		$headers 	= "MIME-Version: 1.0" . "\r\n";
+		$headers   .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+		$headers   .= "From: {$name} <no-reply@ryanmnoble.com>" . "\r\n";
 		$headers   .= "Reply-To: {$from}" . "\r\n";
 
 
